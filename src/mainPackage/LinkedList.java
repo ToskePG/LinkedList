7c8fa34d23a667dc153b30bd9975ddaf2f44f544
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-12-02 15:29:58 +0100

    Dodali funkciju za dodavanje nove glave.

--- src/mainPackage/LinkedList.java
+++ src/mainPackage/LinkedList.java
@@ -1,5 +1,31 @@
 package mainPackage;
 
 public class LinkedList {
+	
+	private Node head;
+	
+	public LinkedList() {
+		this.head = null;
+	}
 
+	public Node getHead() {
+		return head;
+	}
+
+	public void setHead(Node head) {
+		this.head = head;
+	}
+	
+	// Insert functions
+	public boolean insertAtBeggining(int data) {
+		Node newNode = new Node(data);
+		// Novi node mora da pokazuje na trenutnu glavu.
+		newNode.setNext(head);
+		// Postavljamo novu glavu liste
+		head = newNode;
+		return true;
+	}
+	
+	
+	
 }
--- src/mainPackage/Node.java
+++ src/mainPackage/Node.java
@@ -1,5 +1,29 @@
 package mainPackage;
 
 public class Node {
+	
+	private int data;
+	private Node next;
+	
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+	public int getData() {
+		return this.data;
+	}
+	
+	public Node getNext() {
+		return this.next;
+	}
 
 }
