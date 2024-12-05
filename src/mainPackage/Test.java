f3ac0e3c9eabf57023fe683fb3673800a9e25b07
Danilo Tosic <54001767+ToskePG@users.noreply.github.com>
2024-12-05 01:47:59 +0100

    Finishes stack structure.

--- src/mainPackage/Test.java
+++ src/mainPackage/Test.java
@@ -43,11 +43,38 @@ public class Test {
 		l1.displayList();
 		System.out.println("-------------------"); */
 		
-		System.out.println("Kurcina");
 		
 		Stack playerStack = new Stack();
 		
+		Player p1 = new Player("user1", "pass1", 10, 10);
+		Player p2 = new Player("user2", "pass2", 20, 20);
+		Player p3 = new Player("user3", "pass3", 30, 30);
 		
+		playerStack.displayStack();
+		playerStack.pop();
+		System.out.println("----------------------------");
+		
+		playerStack.push(p1);
+		playerStack.displayStack();
+		System.out.println("----------------------------");
+		
+		playerStack.pop();
+		playerStack.displayStack();
+		System.out.println("----------------------------");
+		
+		playerStack.push(p1);
+		playerStack.push(p2);
+		playerStack.push(p3);
+		playerStack.displayStack();
+		System.out.println("----------------------------");
+		
+		playerStack.pop();
+		playerStack.displayStack();
+		System.out.println("----------------------------");
+		
+		
+		playerStack.peek();
+		System.out.println("----------------------------");
 
 	}
 
